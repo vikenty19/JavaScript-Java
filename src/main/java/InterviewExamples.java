d33ac36f5527d58ac307b5c76524b15d89107805
vikenty19 <vikenty.plakhov@testpro.io>
2025-10-20 18:05:56 -0400

    .

--- src/main/java/InterviewExamples.java
+++ src/main/java/InterviewExamples.java
@@ -16,13 +16,34 @@ public class InterviewExamples {
         }
 
     } System.out.println(maxDelta);
+        // Task with aster
+        String aster ="*";
+        for (int i =0;i<5;i++){
+            System.out.println(aster);
+            aster=aster+("*");
+        }
+        System.out.println("*");
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+                System.out.print("\t");
+
+            }
+            System.out.println();
+
+        }
+
+
+        ///////sort array
         int[]c ={1,4,4,15,17};
         int[]d = {6,4,3,7,17};
+
         System.out.println(Arrays.toString(createArray(c,d)));
         for (Object temp:createArray(c,d)){
             System.out.println(temp);
         }
-        System.out.println(Arrays.toString(sortArray()));
+        int[]cd ={1,20,4,15,17};
+        System.out.println(Arrays.toString(sortArrayDesc(cd)));
      /*   for (Object temp:sortArray()){
             System.out.println(temp);
         }*/
@@ -43,18 +64,25 @@ public class InterviewExamples {
 
     }
 
-    public  static int[] sortArray(){
-        int[]c ={1,20,4,15,17};
-
+    public  static int[] sortArray(int[]c){
         for (int i=0;i<c.length-1;i++){
            for(int j =0;j<c.length-1-i;j++){
-
                 if (c[j] > c[j+1]) {
                   int temp = c[j];
                    c[j] =c[j+1];
                    c[j+1]=temp;
-
-
+                }
+            }
+        }
+        return  c;
+    }
+    public  static int[] sortArrayDesc(int[]c){
+        for (int i=0;i<c.length-1;i++){
+            for(int j =0;j<c.length-1-i;j++){
+                if (c[j] < c[j+1]) {
+                    int temp = c[j];
+                    c[j] =c[j+1];
+                    c[j+1]=temp;
                 }
             }
         }
